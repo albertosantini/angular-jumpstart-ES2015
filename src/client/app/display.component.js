98fc63d8a51b21055f7df42b4f13dc3221558eff
Alberto Santini <albertosantini@gmail.com>
2016-01-12 09:45:47 +0100

    Add button to display component.

--- src/client/app/display.component.js
+++ src/client/app/display.component.js
@@ -19,13 +19,18 @@
                 "<p>Friends:</p>" +
                 "<ul>" +
                 "<li *ngFor='#name of names'>{{ name }}</li>" +
-                "</ul>"
+                "</ul>" +
+                "<button (click)='theAnswer(42)'>And the answer is...</button>"
         })
         .Class({
             constructor: [FriendsS, function Display(friendsSrv) {
                 this.me = "Alice";
                 this.names = friendsSrv.getNames();
-            }]
+            }],
+
+            theAnswer: function (res) {
+                alert(res);
+            }
         });
 
 }(window.app || (window.app = {})));

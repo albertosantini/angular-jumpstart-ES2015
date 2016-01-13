88aa7b3ae8129ad8c4c6191626236ac70f5dd6ba
Alberto Santini <albertosantini@gmail.com>
2016-01-13 13:37:52 +0100

    Add routes file in app folder.

---
+++ src/client/app/app.routes.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function (app) {
+    app.View1 = ng.core
+        .Component({
+            template: "<p>View {{ viewName }} loaded with the router</p>"
+        })
+        .Class({
+            constructor: [function Start() {
+                this.viewName = "View1";
+            }]
+
+        });
+
+}(window.app || (window.app = {})));
+
+(function (app) {
+    app.MyRoutes = ng.core
+        .Component({
+            selector: "my-routes",
+            bindings: [ng.router.ROUTER_PROVIDERS]
+        })
+        .View({
+            directives: [ng.router.ROUTER_DIRECTIVES],
+            template: "<router-outlet></router-outlet>"
+        })
+        .Class({
+            constructor: function () {}
+        });
+
+}(window.app || (window.app = {})));
+
+(function (app) {
+    ng.router
+        .RouteConfig([
+            { path: "/", component: app.View1 }
+        ])(app.MyRoutes);
+
+}(window.app || (window.app = {})));

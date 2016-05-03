f322e58c7cfff6939a8893a76ea216c982a5cd45
Alberto Santini <albertosantini@gmail.com>
2016-05-03 11:14:21 +0200

    Update components boot to new api in 2.0.0-rc.0.

--- src/client/app/boot.js
+++ src/client/app/boot.js
@@ -2,9 +2,9 @@
 
 (function (app) {
     document.addEventListener("DOMContentLoaded", function () {
-        ng.platform.browser.bootstrap(app.AppComponent);
-        ng.platform.browser.bootstrap(app.DisplayComponent);
-        ng.platform.browser.bootstrap(app.HelloComponent);
-        ng.platform.browser.bootstrap(app.MyRoutes);
+        ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+        ng.platformBrowserDynamic.bootstrap(app.DisplayComponent);
+        ng.platformBrowserDynamic.bootstrap(app.HelloComponent);
+        ng.platformBrowserDynamic.bootstrap(app.MyRoutes);
     });
 }(window.app || (window.app = {})));

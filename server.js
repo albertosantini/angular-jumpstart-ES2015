1d76fc85a4d6a3faa3cb02b17989e92d06684cc4
icebox <albertosantini@gmail.com>
2017-01-26 18:33:40 +0100

    Improve linting rules.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var path = require("path");
-var express = require("express");
+"use strict";
 
-var app = express();
+const path = require("path");
+const express = require("express");
+
+const app = express();
 
 app.use("/", express.static(path.join(__dirname, "src/client")));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
-app.listen(8000, function () {
+app.listen(8000, () => {
+
+    /* eslint no-console: "off"*/
     console.log("Listening on http://localhost:8000");
 });

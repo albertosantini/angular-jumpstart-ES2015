b2e6243f8e84a73a528be7d433bc713547929535
icebox <albertosantini@gmail.com>
2017-11-30 19:21:00 +0100

    Fix bundle ES2015.

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,17 @@
 "use strict";
 
-const nodeResolve = require("@oasisdigital/rollup-plugin-node-resolve");
+const nodeResolve = require("rollup-plugin-node-resolve");
 const commonjs = require("rollup-plugin-commonjs");
 
 module.exports = {
-    entry: "./src/client/app-es2015/boot.js",
-    dest: "./build/app.bundle.js",
-    format: "iife",
-    moduleName: "app",
+    input: "./src/client/app-es2015/boot.js",
+    output: {
+        file: "./build/app.bundle.js",
+        format: "iife"
+    },
+    name: "app",
     plugins: [
-        nodeResolve({
-            es2015: true,  // Use new Angular es2015.
-            module: false, // Skip the ES5-in-ES2015 modules we aren"t using.
-            browser: true  // Not needed for this example, needed for certain libs
-        }),
+        nodeResolve(),
         commonjs({
 
             // Make it possible to find these individual intra-package files
